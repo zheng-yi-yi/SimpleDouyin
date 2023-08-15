7876d451cad6473ae0c595dbc6141fa88ed27dab
czwzy2017 <879136022@qq.com>
2023-08-15 21:35:04 +0800

    feat:message_controller，使用demo的

--- controllers/message_controller.go
+++ controllers/message_controller.go
@@ -1,15 +1,70 @@
 package controllers
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync/atomic"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/zheng-yi-yi/simpledouyin/models"
 )
+
+var tempChat = map[string][]models.Message{}
+
+var messageIdSequence = int64(1)
+
+type ChatResponse struct {
+	Response
+	MessageList []models.Message `json:"message_list"`
+}
 
 // 发送消息
 func MessageAction(c *gin.Context) {
+	token := c.Query("token")
+	toUserId := c.Query("to_user_id")
+	content := c.Query("content")
 
+	if user, exist := UsersLoginInfo[token]; exist {
+		userIdB, _ := strconv.Atoi(toUserId)
+		chatKey := genChatKey(int64(user.ID), int64(userIdB))
+		atomic.AddInt64(&messageIdSequence, 1)
+		curMessage := models.Message{
+			Id:         messageIdSequence,
+			Content:    content,
+			CreateTime: time.Now().Format(time.Kitchen),
+		}
+
+		if messages, exist := tempChat[chatKey]; exist {
+			tempChat[chatKey] = append(messages, curMessage)
+		} else {
+			tempChat[chatKey] = []models.Message{curMessage}
+		}
+		c.JSON(http.StatusOK, Response{StatusCode: 0})
+	} else {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+	}
 }
 
-// 聊天记录
+// 消息记录
 func MessageChat(c *gin.Context) {
+	token := c.Query("token")
+	toUserId := c.Query("to_user_id")
 
+	if user, exist := UsersLoginInfo[token]; exist {
+		userIdB, _ := strconv.Atoi(toUserId)
+		chatKey := genChatKey(int64(user.ID), int64(userIdB))
+
+		c.JSON(http.StatusOK, ChatResponse{Response: Response{StatusCode: 0}, MessageList: tempChat[chatKey]})
+	} else {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+	}
+}
+
+func genChatKey(userIdA int64, userIdB int64) string {
+	if userIdA > userIdB {
+		return fmt.Sprintf("%d_%d", userIdB, userIdA)
+	}
+	return fmt.Sprintf("%d_%d", userIdA, userIdB)
 }
