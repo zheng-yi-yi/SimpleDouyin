a861661016caf5f5f97e2fe5bbf2ccbbf21db2a7
zhengyiyi <zrh182278@outlook.com>
2023-08-24 17:00:08 +0800

    Fix CommentAction function bug

--- controllers/9_commentAction.go
+++ controllers/9_commentAction.go
@@ -1,58 +1,53 @@
 package controllers
 
 import (
-	"fmt"
-	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zheng-yi-yi/simpledouyin/config"
+	"github.com/zheng-yi-yi/simpledouyin/models"
 )
 
-// 评论操作，不允许发送空评论,删除操作仅自身可以删除或者视频作者可以删除
+// CommentAction 处理评论操作的请求。
 func CommentAction(c *gin.Context) {
-	token, action_type := c.Query("token"), c.Query("action_type")
-	video_id, _ := strconv.ParseInt(c.Query("video_id"), 10, 64)
-	user := UsersLoginInfo[token]
+	userId := c.GetUint("userID")
+	if userId == 0 {
+		Failed(c, "用户不存在")
+		return
+	}
+	action_type := c.Query("action_type")
+	video_id, err := strconv.ParseInt(c.Query("video_id"), 10, 64)
+	if err != nil {
+		Failed(c, err.Error())
+		return
+	}
+	// 发布评论
 	if action_type == "1" {
 		content := c.Query("comment_text")
-		comment, err := commentService.CreateComment(uint(video_id), content, uint(user.ID))
-		if err != nil {
-			fmt.Print(111)
-			c.JSON(http.StatusOK, CommentActionResponse{
-				Response: Response{
-					StatusCode: 1,
-					StatusMsg:  "不允许发送空评论!",
-				},
-				Comment: Comment{},
-			})
+		if err := commentService.CreateComment(uint(video_id), content, userId); err != nil {
+			Failed(c, err.Error())
+			return
 		}
-		c.JSON(http.StatusOK, CommentActionResponse{
-			Response: Response{
-				StatusCode: 0,
-				StatusMsg:  "success",
-			},
-			Comment: Comment{
-				Id: int64(comment.ID),
-				User: User{
-					Id:     int64(user.ID),
-					Name:   user.UserName,
-					Avatar: config.AvatarURL,
-				},
-				Content:    comment.Content,
-				CreateDate: comment.CreatedAt.Format("01-02"),
-			},
-		})
-	} else {
-		comment_id, _ := strconv.ParseInt(c.Query("comment_id"), 10, 64)
-		err := commentService.DeleteCommentById(user.ID, uint(comment_id))
-		if err != nil {
-			c.JSON(http.StatusOK, CommentActionResponse{
-				Response: Response{
-					StatusCode: 1,
-					StatusMsg:  "无权限删除该评论",
-				},
-			})
+		if err := models.IncrementCommentCount(config.DB, uint(video_id)); err != nil {
+			Failed(c, err.Error())
+			return
 		}
+		Success(c, "成功添加一条评论")
+		return
 	}
+	// 删除评论
+	comment_id, parseCommentIdErr := strconv.ParseInt(c.Query("comment_id"), 10, 64)
+	if parseCommentIdErr != nil {
+		Failed(c, parseCommentIdErr.Error())
+		return
+	}
+	if err := commentService.DeleteCommentById(userId, uint(video_id), uint(comment_id)); err != nil {
+		Failed(c, err.Error())
+		return
+	}
+	if err := models.DecreaseCommentCount(config.DB, uint(video_id)); err != nil {
+		Failed(c, err.Error())
+		return
+	}
+	Success(c, "成功删除一条评论")
 }
--- services/comment_service.go
+++ services/comment_service.go
@@ -3,12 +3,28 @@ package services
 import (
 	"errors"
 	"strconv"
+	"time"
 
 	"github.com/zheng-yi-yi/simpledouyin/config"
 	"github.com/zheng-yi-yi/simpledouyin/models"
 )
 
 type CommentService struct {
+}
+
+// CreateComment: 创建新评论
+func (s *CommentService) CreateComment(video_id uint, content string, user_id uint) error {
+	if len(content) == 0 {
+		return errors.New("不能插入空评论！")
+	}
+	comment := models.Comment{
+		VideoId:   video_id,
+		Content:   content,
+		UserId:    user_id,
+		CreatedAt: time.Now(),
+		Cancel:    0,
+	}
+	return config.DB.Create(&comment).Error
 }
 
 // 获取所有未删除的评论
@@ -23,20 +39,6 @@ func (s *CommentService) GetVideoComment(video_id string) ([]models.Comment, err
 	return *Comments, nil
 }
 
-// 往DB中插入相应的评论。另外to_user_id似乎没用到，所以没设置
-func (s *CommentService) CreateComment(video_id uint, content string, user_id uint) (models.Comment, error) {
-	if len(content) == 0 { //不允许空评论
-		return models.Comment{}, errors.New("插入空评论！")
-	}
-	comment := models.Comment{
-		Content: content,
-		VideoId: video_id,
-		UserId:  user_id,
-	}
-	config.DB.Create(&comment)
-	return comment, nil
-}
-
 // 根据相应的评论获取ID
 func (s *CommentService) GetCommentById(comment_id int64) models.Comment {
 	var comment models.Comment
@@ -44,12 +46,7 @@ func (s *CommentService) GetCommentById(comment_id int64) models.Comment {
 	return comment
 }
 
-func (s *CommentService) DeleteCommentById(user_id uint, comment_id uint) error {
-	var comment models.Comment
-	comment = s.GetCommentById(int64(comment_id))
-	if comment.UserId == user_id {
-		config.DB.Where("id = ?", comment_id).Delete(&comment)
-		return nil
-	}
-	return errors.New("无权限删除该评论!")
+// 根据用户ID、视频ID和评论ID，定位并删除对应评论
+func (s *CommentService) DeleteCommentById(userId, videoId, commentId uint) error {
+	return config.DB.Where("user_id=? and video_id=? and id=?", userId, videoId, commentId).Delete(&models.Comment{}).Error
 }
