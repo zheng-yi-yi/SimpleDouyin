4b0dabcbdd6144dc06adf50fbc44025f3b7b0913
zhengyiyi <zrh182278@outlook.com>
2023-08-24 11:16:08 +0800

    feat(utils): add GetCoverName function

--- utils/videoPublish.go
+++ utils/videoPublish.go
@@ -18,3 +18,14 @@ func GetVideoName(userId uint) string {
 	filename := fmt.Sprintf("%d_%d", userId, VideoCount+1)
 	return filename
 }
+
+// GetCoverName 根据用户ID生成封面图片的文件名
+func GetCoverName(userId uint) string {
+	VideoCount, err := models.GetVideoCount(config.DB, userId)
+	if err != nil {
+		log.Printf("无法获取用户的视频个数")
+		return ""
+	}
+	coverName := fmt.Sprintf("%d_%d.png", userId, VideoCount+1)
+	return coverName
+}
