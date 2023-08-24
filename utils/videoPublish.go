d01be9d88fc07db8c8be748e391fd458367113c4
zhengyiyi <zrh182278@outlook.com>
2023-08-24 11:48:03 +0800

    Add function GetVideoDst

--- utils/videoPublish.go
+++ utils/videoPublish.go
@@ -3,10 +3,24 @@ package utils
 import (
 	"fmt"
 	"log"
+	"mime/multipart"
+	"path/filepath"
 
 	"github.com/zheng-yi-yi/simpledouyin/config"
 	"github.com/zheng-yi-yi/simpledouyin/models"
 )
+
+// GetVideoDst 根据文件信息和用户ID生成视频文件的目标路径。
+func GetVideoDst(file *multipart.FileHeader, userId uint) string {
+	videoName := GetVideoName(userId)
+	if videoName == "" {
+		return ""
+	}
+	fileExt := filepath.Ext(file.Filename)
+	filename := videoName + fileExt
+	videoDst := filepath.Join("./public/videos/", filename)
+	return videoDst
+}
 
 // GetVideoName 根据userId_用户发布的视频数量+1
 func GetVideoName(userId uint) string {
