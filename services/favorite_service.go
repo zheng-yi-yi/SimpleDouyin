d37a7ac2c162f482bfda5d7b6512db3eaf9ee4b7
zhengyiyi <zrh182278@outlook.com>
2023-08-26 08:00:16 +0800

    perf: optimize IsFavorite function

--- services/favorite_service.go
+++ services/favorite_service.go
@@ -8,9 +8,14 @@ import (
 type FavoriteService struct {
 }
 
-// 判断用户是否点赞了视频
+// IsFavorite ，判断用户是否点赞了视频
 func (s *FavoriteService) IsFavorite(userId, videoId uint) bool {
+	// 创建一个 Favorite 结构体实例，用于存储查询结果
 	var favorite models.Favorite
-	err := config.DB.Where("user_id = ? AND video_id = ? AND status = 1", userId, videoId).First(&favorite).Error
-	return err == nil
+
+	// 在数据库中查找匹配的点赞记录
+	result := config.DB.Where("user_id = ? AND video_id = ? AND status = 1", userId, videoId).First(&favorite)
+
+	// 检查是否找到匹配的点赞记录
+	return result.Error == nil
 }
