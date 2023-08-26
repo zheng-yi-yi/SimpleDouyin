d93eefca907c1c47efa72d3f4da6c866cdbae243
zhengyiyi <zrh182278@outlook.com>
2023-08-26 08:20:52 +0800

    feat: add CancelLike function in service layer

--- services/favorite_service.go
+++ services/favorite_service.go
@@ -69,3 +69,45 @@ func (s *FavoriteService) AddLike(userId, videoId uint) error {
 
 	return nil
 }
+
+// CancelLike ，取消点赞操作
+func (s *FavoriteService) CancelLike(userId, videoId uint) error {
+	// 获取数据库连接实例
+	db := config.DB
+
+	// 创建 Favorite 对象用于存储查询结果
+	var favorite models.Favorite
+
+	// 在数据库中查找匹配的点赞记录
+	err := db.Where("user_id = ? AND video_id = ? AND status = 1", userId, videoId).First(&favorite).Error
+	if err != nil {
+		return err // 如果发生错误，返回错误信息
+	}
+
+	// 将点赞状态设为 0，表示取消点赞
+	favorite.Status = 0
+
+	// 更新数据库中的点赞记录
+	err = db.Save(&favorite).Error
+	if err != nil {
+		return err // 如果更新发生错误，返回错误信息
+	}
+
+	// 获取该点赞视频的用户id
+	author_id, err := models.GetAuthorIDForVideo(db, uint(videoId))
+	if err != nil {
+		return err
+	}
+
+	// 成功取消点赞后，调用 DecrementUserLikeCount 函数，将用户的点赞数减一
+	if err := models.DecrementUserLikeCount(db, author_id); err != nil {
+		return err
+	}
+
+	// 成功取消点赞后，调用 IncrementVideoLikeCount 函数，将视频的获赞数加一
+	if err := models.DecrementVideoLikeCount(db, uint(videoId)); err != nil {
+		return err
+	}
+
+	return nil // 操作成功，返回 nil 表示没有错误
+}
