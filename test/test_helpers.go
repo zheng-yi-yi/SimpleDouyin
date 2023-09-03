3f62e76c6dfcfe33f1d0ff7eb5c7781665d005f8
zhengyiyi <zrh182278@outlook.com>
2023-09-03 22:36:30 +0800

    Add generateRandomUsername and generateRandomPassword functions for generating random usernames and passwords

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -1,8 +1,11 @@
 package test
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/gavv/httpexpect/v2"
 	"github.com/zheng-yi-yi/simpledouyin/config"
@@ -51,4 +54,26 @@ func getTestUserIdAndToken(username string, password string, e *httpexpect.Expec
 		userId = int(registerResp.Value("user_id").Number().Raw())
 	}
 	return userId, token
+}
+
+// generateRandomUsername ， 生成随机用户名
+func generateRandomUsername() string {
+	// 定义可用字符集合
+	characters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	source := rand.NewSource(time.Now().UnixNano())
+	randGen := rand.New(source)
+	// 生成随机8个字符的用户名
+	username := make([]byte, 8)
+	for i := 0; i < 8; i++ {
+		username[i] = characters[randGen.Intn(len(characters))]
+	}
+	return string(username)
+}
+
+// generateRandomPassword ， 生成随机六位数密码
+func generateRandomPassword() string {
+	source := rand.NewSource(time.Now().UnixNano())
+	randGen := rand.New(source)
+	password := fmt.Sprintf("%06d", randGen.Intn(1000000))
+	return password
 }
