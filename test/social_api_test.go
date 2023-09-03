ba67361f64f03520adb0e2d232761e070b4940c6
zhengyiyi <zrh182278@outlook.com>
2023-09-03 22:42:06 +0800

    Add TestChat function to test Message module

--- test/social_api_test.go
+++ test/social_api_test.go
@@ -68,3 +68,44 @@ func TestRelation(t *testing.T) {
 	// 使用断言确保测试用户 A 在粉丝列表中
 	assert.True(t, containTestUserA, "关注者测试用户失败")
 }
+
+// 测试 “消息” 模块
+func TestChat(t *testing.T) {
+
+	e := newExpect(t)
+
+	// 获取测试用户A的用户 ID 和 token
+	userIdA, tokenA := getTestUserIdAndToken("ChatTestA", "881246", e)
+	// 获取测试用户B的用户 ID 和令牌
+	userIdB, tokenB := getTestUserIdAndToken("ChatTestB", "958831", e)
+
+	// 发送 POST 请求来发送消息给用户 B
+	messageResp := e.POST("/douyin/message/action/").
+		WithQuery("token", tokenA).WithQuery("to_user_id", userIdB).WithQuery("action_type", 1).WithQuery("content", "Send to UserB").
+		WithFormField("token", tokenA).WithFormField("to_user_id", userIdB).WithFormField("action_type", 1).WithQuery("content", "Send to UserB").
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+	messageResp.Value("status_code").Number().IsEqual(0) // 确保发送消息操作成功
+
+	// 发送 GET 请求来获取用户 A 和用户 B 之间的聊天消息列表
+	chatResp := e.GET("/douyin/message/chat/").
+		WithQuery("token", tokenA).WithQuery("to_user_id", userIdB).
+		WithFormField("token", tokenA).WithFormField("to_user_id", userIdB).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+	chatResp.Value("status_code").String().IsEqual("0") // 确保获取聊天消息列表成功
+	chatResp.Value("message_list").Array().NotEmpty()   // 确保聊天消息列表不为空
+
+	// 发送 GET 请求来获取用户 B 和用户 A 之间的聊天消息列表
+	chatResp = e.GET("/douyin/message/chat/").
+		WithQuery("token", tokenB).WithQuery("to_user_id", userIdA).
+		WithFormField("token", tokenB).WithFormField("to_user_id", userIdA).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+	chatResp.Value("status_code").String().IsEqual("0") // 确保获取聊天消息列表成功
+	chatResp.Value("message_list").Array().NotEmpty()   // 确保聊天消息列表不为空
+
+}
